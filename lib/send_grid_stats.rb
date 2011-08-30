cb58ec344c24a806629547e47e72d96f75259ee7
Eric Barendt <ebarendt@gmail.com>
2011-08-29 22:03:30 -0700

    initial sendgrid app

---
+++ lib/send_grid_stats.rb
@@ -0,0 +1,44 @@
+require 'sendgrid_toolkit'
+
+class SendGridStats
+
+  def initialize
+    @api_user = ENV["SENDGRID_USER"]
+    @api_key = ENV["SENDGRID_PASS"]
+  end
+
+  def load_bounces
+    bounces = SendgridToolkit::Bounces.new(@api_user, @api_key)
+    bounces.retrieve_with_timestamps
+  end
+
+  def load_stats
+    stats = SendgridToolkit::Statistics.new(@api_user, @api_key)
+    stats.retrieve
+  end
+
+end
+
+sendgrid = SendGridStats.new
+stats = sendgrid.load_stats
+#print "stats: #{stats[0]}\n"
+#print "#{stats[0].keys}\n"
+if stats[0]["bounces"]
+  num_bounces = stats[0]["bounces"]
+  print "#{num_bounces} bounces\n"
+  bounces = sendgrid.load_bounces
+  bounces.each do |bounce|
+    email = bounces["email"]
+    status = bounces["status"]
+    reason = bounces["reason"]
+    created = bounces["created"]
+    print """
+--------------------
+Bounced at: #{created}
+Email: #{email}
+Status: #{status}
+Reason: #{reason}
+--------------------
+"""
+  end
+end
